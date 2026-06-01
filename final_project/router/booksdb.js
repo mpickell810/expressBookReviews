79aa3b3ef29d37a750fafad4c474277b13a06db1
mpickell810 <silver066surfer@gmail.com>
2026-06-01 16:08:59 -0400

    updating js files for final project

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "9741400000001" },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "9741400000002" },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "9741400000003" },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "9741400000004" },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "9741400000005" },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "9741400000006" },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "9741400000007" },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "9741400000008" },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "9741400000009" },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "9741400000010" }
+const books = {
+    9741400000001: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+    9741400000002: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+    9741400000003: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+    9741400000004: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+    9741400000005: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+    9741400000006: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+    9741400000007: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+    9741400000008: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    9741400000009: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+    9741400000010: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
