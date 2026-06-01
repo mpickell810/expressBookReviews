419f66e95324f6ef0b638b35aeae632b3689b5e0
mpickell810 <silver066surfer@gmail.com>
2026-06-01 15:28:40 -0400

    updating general.js for final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,31 +14,67 @@ public_users.post("/register", (req,res) => {
 public_users.get('/',function (req, res) {
   //  Send JSON response with formatted book data
   res.send(JSON.stringify(books,null,3));
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Multiple books have been found, please choose a book."});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the ISBN parameter from the request URL and send the corresponding ISBN's details
+  const isbn = req.params.isbn;
+  const book = books[isbn]; 
+  
+  if (book) {
+  // Return immediately so the function ends here
+  return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+  // Return immediately if not found
+    res.status(404).send("Book not found.");
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the author parameter from the request URL and send the corresponding author details
+  const author = req.params.author;
+  const book = books[author];
+
+  if (book) {
+    // Return immediately so the function ends here
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    // Return immediately if not found
+  return res.status(404).send("Book not found.");
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //  Retrieve the title parameter from the request URL and send the corresponding title details
+  const title = req.params.title;
+  const book = books[title];
+
+  if (book) {
+    // Return immediately so the function ends here
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    // Return immediately if not found
+  return res.status(404).send("Book not found.");
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Retrieve the review parameter from the request URL and send the corresponding review details
+  const review = req.params.title;
+  const book = books[review];
+
+  if (book) {
+    // Return immediately so the function ends here
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    // Return immediately if not found
+  return res.status(404).send("No reviews found for this book.");
+  }
 });
 
 module.exports.general = public_users;
