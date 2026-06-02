c87442d66586e37ae94390fac2e9142007a5fb84
mpickell810 <silver066surfer@gmail.com>
2026-06-02 03:50:50 -0400

    updating gneral.js for project tasks 2-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,15 +35,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   // Retrieve the author parameter from the request URL and send the corresponding author details
-  const author = req.params.author;
-  const book = books[author];
+  const author = req.params.author.toLowerCase().trim();
+  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().trim() === author);
 
-  if (book) {
+  if (booksByAuthor.length > 0) {
     // Return immediately so the function ends here
-    return res.status(200).send(JSON.stringify(book, null, 4));
+    return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
   } else {
     // Return immediately if not found
-  return res.status(404).send("Book not found.");
+  return res.status(404).send("No books found for this author.");
   }
 });
 
@@ -51,26 +51,26 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
   //  Retrieve the title parameter from the request URL and send the corresponding title details
   const title = req.params.title;
-  const book = books[title];
+  const booksByTitle = Object.values(books).filter(book => book.title === title);
 
-  if (book) {
+  if (booksByTitle.length > 0) {
     // Return immediately so the function ends here
-    return res.status(200).send(JSON.stringify(book, null, 4));
+    return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
   } else {
     // Return immediately if not found
-  return res.status(404).send("Book not found.");
+  return res.status(404).send("No books found with this title.");
   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Retrieve the review parameter from the request URL and send the corresponding review details
-  const review = req.params.title;
-  const book = books[review];
+  const isbn = req.params.isbn;
+  const book = books[isbn];
 
   if (book) {
     // Return immediately so the function ends here
-    return res.status(200).send(JSON.stringify(book, null, 4));
+    return res.status(200).send(JSON.stringify(book.reviews, null, 4));
   } else {
     // Return immediately if not found
   return res.status(404).send("No reviews found for this book.");
