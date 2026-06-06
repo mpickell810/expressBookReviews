3ed2e4179826e452441fb2ab2b890593066a87f8
mpickell810 <silver066surfer@gmail.com>
2026-06-06 16:07:37 -0400

    adding book review for AI grader

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -6,7 +6,7 @@ const books = {
     9741400000005: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
     9741400000006: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
     9741400000007: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-    9741400000008: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    9741400000008: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {"user1": "A tale about how Mr. Darcey got his revenge."} },
     9741400000009: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
     9741400000010: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
